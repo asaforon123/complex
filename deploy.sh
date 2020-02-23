067bf36c951bf8a1b9bd7ca42ee8b61750d27219
Asaf Oron <asaforon@Asafs-MacBook-Pro.local>
2020-02-23 17:29:38 +0200

    fixes docker container problems

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+echo "start deploy"
 docker build -t oronasaf123/mulit-client:latest  -t oronasaf123/mulit-client:$SHA -f ./client/Dockerfile ./client
 docker build -t oronasaf123/multi-server:latest  -t oronasaf123/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t oronasaf123/multi-worker:latest  -t oronasaf123/multi-worker:$SHA-f ./worker/Dockerfile ./worker
@@ -11,3 +12,4 @@ kubectl apply -f k8s
 kubectl set image deployment/client-deployment client=oronasaf123/multi-client:$SHA
 kubectl set image deployment/server-deployment server=oronasaf123/multi-server:$SHA
 kubectl set image deployment/worker-deployment worker=oronasaf123/multi-worker:$SHA
+echo "end deploy"
