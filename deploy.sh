43846426d3d7682d4f9d9fffb6ca25678c3b8497
Asaf Oron <asaforon@Asafs-MacBook-Pro.local>
2020-02-23 15:26:00 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t oronasaf123/mulit-client:latest  -t oronasaf123/mulit-client:$SHA -f ./client/Dockerfile ./client
+docker build -t oronasaf123/multi-server:latest  -t oronasaf123/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t oronasaf123/multi-worker:latest  -t oronasaf123/multi-worker:$SHA-f ./worker/Dockerfile ./worker
+docker push oronasaf123/multi-client:latest
+docker push oronasaf123/multi-server:latest
+docker push oronasaf123/multi-worker:latest
+docker push oronasaf123/multi-client:$SHA
+docker push oronasaf123/multi-server:$SHA
+docker push oronasaf123/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployment/client-deployment client=oronasaf123/multi-client:$SHA
+kubectl set image deployment/server-deployment server=oronasaf123/multi-server:$SHA
+kubectl set image deployment/worker-deployment worker=oronasaf123/multi-worker:$SHA
